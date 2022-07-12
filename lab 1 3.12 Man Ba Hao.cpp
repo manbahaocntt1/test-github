b15e65e1b419fdbc97858dddb2e875c6b7a601f0
unknown <manbahao1508@gmail.com>
2022-07-12 12:51:03 +0700

    first commit

---
+++ lab 1 3.12 Man Ba Hao.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h> 
+using namespace std; 
+int main() 
+{
+	float c=0,f=0;
+	cout<< "Nhap do F:";cin>>f;
+	c=(5.0/9.0)*(f-32);
+	cout<<"Hien thi do c:"<<c;
+	return 0;
+	}
